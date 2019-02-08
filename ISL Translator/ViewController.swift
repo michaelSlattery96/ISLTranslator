e6e1ff7ef22ca85e804b1f2423370b05b8ecfdaf
Michael Slattery <michael.slattery@teamwork.com>
2019-02-08 17:15:28 +0000

    Initial commit

---
+++ ISL Translator/ViewController.swift
@@ -0,0 +1,158 @@
+//
+//  ViewController.swift
+//  ISL Translator
+//
+//  Created by Michael Slattery on 26/09/2018.
+//  Copyright © 2018 Michael Slattery. All rights reserved.
+//
+
+import AVKit
+
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var label: UILabel!
+    
+    lazy var cameraSession: AVCaptureSession = {
+        
+        let captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .medium
+        return captureSession
+    }()
+    
+    lazy var previewLayer: AVCaptureVideoPreviewLayer = {
+        let preview =  AVCaptureVideoPreviewLayer(session: self.cameraSession)
+        preview.bounds = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height)
+        preview.position = CGPoint(x: self.view.bounds.midX, y: self.view.bounds.midY)
+        preview.videoGravity = .resize
+        return preview
+    }()
+    
+    var model = retrained_graph()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let captureDevice = AVCaptureDevice.default(for: .video)!
+        
+        do {
+            let deviceInput = try AVCaptureDeviceInput(device: captureDevice)
+            
+            cameraSession.beginConfiguration()
+            
+            if (cameraSession.canAddInput(deviceInput) == true) {
+                cameraSession.addInput(deviceInput)
+            }
+            
+            let dataOutput = AVCaptureVideoDataOutput()
+            
+            dataOutput.videoSettings = [(kCVPixelBufferPixelFormatTypeKey as NSString) : NSNumber(value: kCVPixelFormatType_420YpCbCr8BiPlanarFullRange as UInt32)] as [String : Any]
+            
+            dataOutput.alwaysDiscardsLateVideoFrames = true
+            
+            if (cameraSession.canAddOutput(dataOutput) == true) {
+                cameraSession.addOutput(dataOutput)
+            }
+            
+            cameraSession.commitConfiguration()
+            
+            let queue = DispatchQueue(label: "com.imageProcessing.Queue")
+            dataOutput.setSampleBufferDelegate(self, queue: queue)
+            
+        }
+        catch let error as NSError {
+            NSLog("\(error), \(error.localizedDescription)")
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        var frame = view.frame
+        frame.size.height = frame.size.height - 35.0
+        previewLayer.frame = frame
+        
+        view.layer.addSublayer(previewLayer)
+        cameraSession.startRunning()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection){
+        
+        connection.videoOrientation = .portrait
+        if let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) {
+            
+            let ciImage = CIImage(cvImageBuffer: imageBuffer)
+            let img = UIImage(ciImage: ciImage).resizeTo(CGSize(width: 224, height: 224))
+            if let uiImage = img {
+                
+                let pixelBuffer = uiImage.buffer()!
+                let output = try? model.prediction(input__0: pixelBuffer)
+                DispatchQueue.main.async {
+                    
+                    self.imageView.image = uiImage
+                    self.label.text = output?.classLabel ?? "I don't know"
+                }
+            }
+        }
+    }
+}
+
+extension UIImage {
+    
+    func buffer() -> CVPixelBuffer? {
+        
+        return UIImage.convert(image: self)
+    }
+    
+    static func convert(image: UIImage) -> CVPixelBuffer? {
+        
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer : CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(image.size.width), Int(image.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        guard (status == kCVReturnSuccess) else {
+            return nil
+        }
+        
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: pixelData, width: Int(image.size.width), height: Int(image.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
+        
+        context?.translateBy(x: 0, y: image.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+        
+        UIGraphicsPushContext(context!)
+        image.draw(in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        
+        return pixelBuffer
+    }
+    
+    func resizeTo(_ size: CGSize) -> UIImage? {
+        UIGraphicsBeginImageContext(size)
+        draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
+}
+
